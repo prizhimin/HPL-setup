903e805527b06a840135ec2fef93cc564e3d2ca4
Nikolay Prizhimin <prinv@yandex.ru>
2025-09-09 22:50:28 +0500

    Update Ryzen-5-7500F-32Gb-AVX512-DDR5-hpl-setup.sh

--- Ryzen-5-7500F-32Gb-AVX512-DDR5-hpl-setup.sh
+++ Ryzen-5-7500F-32Gb-AVX512-DDR5-hpl-setup.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 # https://www.mgaillard.fr/2022/08/27/benchmark-with-hpl.html
 
-sudo apt install -y build-essential hwloc libhwloc-dev libevent-dev gfortran wget mc git btop zlib1g zlib1g-dev libz-dev
+sudo apt install -y build-essential hwloc libhwloc-dev libevent-dev gfortran zlib1g zlib1g-dev libz-dev wget mc git btop
 
 cd ~
 git clone https://github.com/OpenMathLib/OpenBLAS.git
 cd OpenBLAS
 
-# ОПТИМИЗАЦИЯ: Добавлены AVX512 флаги
 make TARGET=ZEN4 USE_OPENMP=1 NUM_THREADS=12
 make PREFIX=$HOME/opt/OpenBLAS install
 
@@ -15,7 +14,6 @@ cd ~
 wget https://download.open-mpi.org/release/open-mpi/v5.0/openmpi-5.0.8.tar.gz
 tar xf openmpi-5.0.8.tar.gz
 cd openmpi-5.0.8
-# ОПТИМИЗАЦИЯ: Добавлены AVX512 и DDR5 флаги
 CFLAGS="-Ofast -march=znver4 -mtune=znver4 -mavx512f -mavx512dq -mavx512vl -mavx512bw -mprefer-vector-width=512" ./configure --prefix=$HOME/opt/OpenMPI
 make -j $(nproc)
 make install
@@ -35,7 +33,7 @@ sudo ln -s /usr/bin/gfortran /usr/bin/g77
 cd ~/hpl
 
 # Создание Makefile для Ryzen 5 7500F
-cat > Make.linux << 'EOF'
+cat > Make.zen4 << 'EOF'
 SHELL        = /bin/sh
 CD           = cd
 CP           = cp
@@ -43,7 +41,7 @@ LN_S         = ln -s
 MKDIR        = mkdir
 RM           = /bin/rm -f
 TOUCH        = touch
-ARCH         = linux
+ARCH         = zen4
 TOPdir       = $(HOME)/hpl
 INCdir       = $(TOPdir)/include
 BINdir       = $(TOPdir)/bin/$(ARCH)
@@ -62,7 +60,6 @@ HPL_OPTS     = -DHPL_CALL_CBLAS
 HPL_DEFS     = $(F2CDEFS) $(HPL_OPTS) $(HPL_INCLUDES)
 CC           = $(MPdir)/bin/mpicc
 CCNOOPT      = $(HPL_DEFS)
-# ОПТИМИЗАЦИЯ: Добавлены AVX512 и DDR5 флаги
 CCFLAGS      = $(HPL_DEFS) -fomit-frame-pointer -Ofast -march=znver4 -mtune=znver4 -mavx512f -mavx512dq -mavx512vl -mavx512bw -mprefer-vector-width=512
 LINKER       = $(MPdir)/bin/mpif77
 LINKFLAGS    = $(CCFLAGS)
@@ -72,16 +69,16 @@ RANLIB       = echo
 EOF
 
 # Компиляция HPL
-make arch=linux
+make arch=zen4
 
 # Создание HPL.dat конфигурации для Ryzen 5 7500F с 32GB памяти
-cat > ~/hpl/bin/linux/HPL.dat << 'EOF'
+cat > ~/hpl/bin/zen4/HPL.dat << 'EOF'
 HPLinpack benchmark input file
 Innovative Computing Laboratory, University of Tennessee
 HPL.out      output file name (if any)
 6            device out (6=stdout,7=stderr,file)
 1            # of problems sizes (N)
-38000        Ns
+43000        Ns
 1            # of NBs
 256          NBs
 0            PMAP process mapping (0=Row-,1=Column-major)
@@ -110,13 +107,13 @@ HPL.out      output file name (if any)
 EOF
 
 echo "Компиляция завершена!"
-echo "Бинарный файл: ~/hpl/bin/linux/xhpl"
-echo "Конфигурационный файл: ~/hpl/bin/linux/HPL.dat"
+echo "Бинарный файл: ~/hpl/bin/zen4/xhpl"
+echo "Конфигурационный файл: ~/hpl/bin/zen4/HPL.dat"
 
 # Запуск теста производительности
 echo "Запуск HPL теста на Ryzen 5 7500F с 32GB памяти..."
 echo "Используется 12 процессов (6 ядер + HT)"
-cd ~/hpl/bin/linux
+cd ~/hpl/bin/zen4
 
 # Экспорт переменных для MPI
 export MPI_HOME=$HOME/opt/OpenMPI
